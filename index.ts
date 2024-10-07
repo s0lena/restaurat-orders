ddff211eea066ab8a565bed146f9231644efcbe3
s0lena <solena.work@gmail.com>
2024-10-07 16:44:53 +0300

    Final solution

--- index.ts
+++ index.ts
@@ -25,7 +25,17 @@ function getOrders(price: PriceBracket, orders: Order[][]) {
   return filterOrders;
 }
 // Add your printOrders() function below:
-
+function printOrders(restaurants: Restaurant[], orders: Order[][]) {
+  restaurants.forEach((restaurant: Restaurant, index: number) => {
+    if (orders[index].length > 0) {
+      console.log(restaurant.name);
+    }
+    orders[index].forEach((order) => {
+      console.log(`${order.name}:${order.price}`);
+    });
+  });
+}
 // Main
-// const eligibleOrders = getOrders(PriceBracket.Low, orders);
-// printOrders(restaurants, eligibleOrders);
+const eligibleOrders = getOrders(PriceBracket.Low, orders);
+
+printOrders(restaurants, eligibleOrders);
